78f75f43e0c309f46370d631d25d4645e7d67967
kaljitism <dwikaljit@gmail.com>
2023-05-23 02:32:31 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tetrisgame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tetrisgame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/values.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+// Grid Dimensions
+int rowLength = 10;
+int columnLength = 17;
+
+// Movement Direction
+enum Direction {
+  left,
+  right,
+  down,
+}
+
+
+// Tetrominos
+enum Tetromino {
+  L,
+  J,
+  I,
+  O,
+  S,
+  Z,
+  T,
+  empty
+}
+
+const Map<Tetromino, Color> tetrominoColors = {
+  Tetromino.L: Colors.orange,
+  Tetromino.J: Colors.blue,
+  Tetromino.I: Colors.pink,
+  Tetromino.O: Colors.yellow,
+  Tetromino.S: Colors.green,
+  Tetromino.Z: Colors.red,
+  Tetromino.T: Colors.purple,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
